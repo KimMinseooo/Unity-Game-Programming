101ad1a129fb4cf62648e8c19021b13a03e4cbf0
KJW1113 <whddnr2729@ks.ac.kr>
2019-12-14 01:21:58 +0900

    시작화면 씬 추가
    시작화면 씬 추가
    i버튼 옮김.
    옵션버튼 기능 추가 예정

---
+++ Assets/Scripts/StartView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartView : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+    // Start is called before the first frame update
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("MainScenes");
+        
+    }
+
+}
