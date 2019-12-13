e0f5c82945bae8b5a5601b59e08c29d8f5e55af3
김민서 <alstj1274@ks.ac.kr>
2019-12-13 16:53:14 +0900

    Added healthbar
    제발 추가가 되어주세요
    체력바 추가
    피격시 체력바 감소
    데미지 텍스트 출력

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,10 +9,18 @@ public class Enemy : MonoBehaviour
     // 유닛 초기 체력
     public float startHealth = 100;
     private float Health;
+    public Image healthBar;
+
+    private bool isDead =false;
     // 유닛 가격
     public int coin = 100;
 
+    public GameObject DamageText;
+    public GameObject TextPos;
 
+
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +30,28 @@ public class Enemy : MonoBehaviour
     }
 
     // 유닛 데미지 함수
-    public void TakeDamage() {
-        PlayerStats.money += coin;
-        Destroy(gameObject);
+    public void TakeDamage(float amount) {
+        
+        GameObject dmgText =Instantiate(DamageText, TextPos.transform.position, Quaternion.Euler(40,0,0));
+        dmgText.GetComponent<Text>().text = amount.ToString();
+        Destroy(dmgText, 1f); //1초 후 데미지 텍스트 삭제
+        
+        Health -=amount;
 
+        healthBar.fillAmount = Health /startHealth;
+
+        if(Health <= 0 && !isDead)
+        {
+        Die();
+        }
+    }
+
+    void Die()
+    {
+        isDead =true;
+
+        PlayerStats.money += coin;
+
+        Destroy(gameObject);
     }
 }
